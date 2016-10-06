8c337a658886291cbe2e3cb288954d84e2cd4db8
Guillaume CAOUISSIN <guillaume.caouissin@gmail.com>
2016-10-06 02:33:29 +0200

    Ajout de la gestion des crons qui ne peuvent s'executer en parallèle

---
+++ src/Starkerxp/StructureBundle/Command/LockCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Starkerxp\StructureBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\LockHandler;
+
+abstract class LockCommand extends ContainerAwareCommand
+{
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+    /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    public function nomLocker()
+    {
+        return $this->getName();
+    }
+
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $lockHandler = new LockHandler($this->nomLocker());
+        if (!$lockHandler->lock()) {
+            $this->output->writeln("<error>Commande déjà lancée !</error>");
+
+            return false;
+        }
+        $this->executeWithLocker($input, $output);
+    }
+
+    public abstract function traitement();
+}
