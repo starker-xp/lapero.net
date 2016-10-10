ef9bbc1b81700d4db1209e3c3729f78a6fe6bd5c
Guillaume CAOUISSIN <guillaume.caouissin@gmail.com>
2016-10-11 01:55:40 +0200

    Ajout d'une base de controller

---
+++ src/Starkerxp/StructureBundle/Controller/CoreController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Starkerxp\StructureBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Starkerxp\CampagneBundle\Services\Render\RenderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class CoreController extends Controller
+{
+    public function getRepository($entityFQCN)
+    {
+        return $this->getEntityManager()->getRepository($entityFQCN);
+    }
+
+    public function getEntityManager()
+    {
+        return $this->getDoctrine()->getManager();
+    }
+
+}
