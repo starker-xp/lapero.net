3b9940604a6dd945ca3544538ee5d81da310a7fa
Guillaume CAOUISSIN <guillaume.caouissin@gmail.com>
2016-11-03 01:37:41 +0100

    On distingue les des controllers mise à jour du composer.json ajout d'informations de routing

---
+++ src/AppBundle/Controller/PanierController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PanierController extends Controller
+{
+
+    public function ajouterAuPanierAction(Request $request)
+    {
+    }
+
+    public function retirerDuPanierAction(Request $request)
+    {
+    }
+
+    public function voirMonPanierAction(Request $request)
+    {
+    }
+
+    public function viderMonPanierAction(Request $request)
+    {
+    }
+
+    public function passerCommandeAction(Request $request)
+    {
+    }
+
+    public function payerLaCommandeAction(Request $request)
+    {
+    }
+
+    public function paiementAEteValideAction(Request $request)
+    {
+    }
+
+    public function paiementAEteRefuseAction(Request $request)
+    {
+    }
+}
--- src/AppBundle/Controller/UtilisateurController.php
+++ src/AppBundle/Controller/UtilisateurController.php
@@ -17,41 +17,11 @@ class UtilisateurController extends Controller
 
     public function supprimerMonCompteAction(Request $request)
     {
+
     }
 
     public function modifierMonProfilAction(Request $request)
     {
     }
 
-    public function ajouterAuPanierAction(Request $request)
-    {
-    }
-
-    public function retirerDuPanierAction(Request $request)
-    {
-    }
-
-    public function voirMonPanierAction(Request $request)
-    {
-    }
-
-    public function viderMonPanierAction(Request $request)
-    {
-    }
-
-    public function passerCommandeAction(Request $request)
-    {
-    }
-
-    public function payerLaCommandeAction(Request $request)
-    {
-    }
-
-    public function paiementAEteValideAction(Request $request)
-    {
-    }
-
-    public function paiementAEteRefuseAction(Request $request)
-    {
-    }
 }
