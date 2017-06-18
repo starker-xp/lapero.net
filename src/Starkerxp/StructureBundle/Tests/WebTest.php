7137663f62e3a3c7f74041a01fc70434d599e658
Guillaume CAOUISSIN <guillaume.caouissin@gmail.com>
2017-06-18 18:22:04 +0200

    Gestion des differentes connexion Gestion des fixtures où le fait de fixer des ids sont necessaires Possibilité de tester des fonctions privée / protected via reflexion

--- src/Starkerxp/StructureBundle/Tests/WebTest.php
+++ src/Starkerxp/StructureBundle/Tests/WebTest.php
@@ -2,8 +2,12 @@
 
 namespace Starkerxp\StructureBundle\Tests;
 
+use Doctrine\ORM\EntityManager;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
-use Starkerxp\UtilisateurBundle\Entity\Utilisateur;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 abstract class WebTest extends WebTestCase
@@ -16,17 +20,14 @@ abstract class WebTest extends WebTestCase
      */
     public function setUp()
     {
+        parent::setUp();
         $this->loadFixtureFiles([]);
     }
 
-    /**
-     * Retourne l'entity manager de la connexion defaut.
-     *
-     * @return \Doctrine\Common\Persistence\ObjectManager|object
-     */
-    public function getEntityManager()
+    public function tearDown()
     {
-        return $this->getContainer()->get('doctrine')->getManager();
+        parent::tearDown();
+        $this->getEntityManager()->getConnection()->close();
     }
 
     public function getAuthClient()
@@ -44,22 +45,114 @@ abstract class WebTest extends WebTestCase
 
         $client = static::createClient();
         $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $dataHeader['token']));
+
         return $client;
     }
 
     /**
      * Generates a URL from the given parameters.
      *
-     * @param string $route         The name of the route
-     * @param mixed  $parameters    An array of parameters
-     * @param int    $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
+     * @param string $route The name of the route
+     * @param mixed $parameters An array of parameters
+     * @param int $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
      *
      * @return string The generated URL
      *
      * @see UrlGeneratorInterface
      */
-    protected function generateUrl($route, $parameters = array(), $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    protected function generateUrl($route, $parameters = [], $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
     {
         return $this->getContainer()->get('router')->generate($route, $parameters, $referenceType);
     }
+
+    /**
+     * @param string|null $name
+     * @return \Doctrine\DBAL\Connection
+     */
+    public function getConnection($name = null)
+    {
+        return $this->getContainer()->get('doctrine')->getConnection($name);
+    }
+
+    /**
+     * @param string|null $name
+     * @return EntityManager
+     */
+    public function getEntityManager($name = null)
+    {
+        $entityManager = $this->getContainer()->get('doctrine')->getManager($name);
+
+        return $entityManager;
+    }
+
+    /**
+     * @param $entiteCQFD
+     * @param string|null $name
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    public function getRepository($entiteCQFD, $name = null)
+    {
+        $repository = $this->getEntityManager($name)->getRepository($entiteCQFD);
+
+        return $repository;
+    }
+
+    protected $idGeneratorTypes = [];
+
+    protected function allowFixedIdsFor(array $entityClasses, $name = null)
+    {
+        foreach ($entityClasses as $entityClass) {
+            $metadata = $this->getEntityManager($name)->getClassMetadata($entityClass);
+            $this->idGeneratorTypes[$entityClass] = $metadata->generatorType;
+            $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
+            $metadata->setIdGenerator(new \Doctrine\ORM\Id\AssignedGenerator());
+        }
+    }
+
+    protected function recoverIdGenerators($name = null)
+    {
+        foreach ($this->idGeneratorTypes as $entityClass => $idGeneratorType) {
+            $metadata = $this->getEntityManager($name)->getClassMetadata($entityClass);
+            $metadata->setIdGeneratorType($idGeneratorType);
+        }
+    }
+
+    public function loadFixtureFiles(array $paths = [], $append = false, $omName = null, $registryName = 'doctrine')
+    {
+        $retour = parent::loadFixtureFiles($paths, $append, $omName, $registryName);
+        $this->recoverIdGenerators();
+        $this->getEntityManager()->clear();
+
+        return $retour;
+    }
+
+    public function executerCommande($command = [])
+    {
+        $container = $this->getContainer();
+        $kernel = $container->get('kernel');
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+        $input = new ArrayInput($command);
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object $object Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod($object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
 }
